8828de9acc1308267330ab4bb72904783803f9cd
Lauren Laskerr <llask@me.com>
2019-08-06 20:41:35 -0400

    result for array-diff, .foreach and .reduce

---
+++ js/array-diff.js
@@ -0,0 +1,32 @@
+/*
+Array.diff
+6kyu
+
+Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+It should remove all values from list a, which are present in list b.
+
+array_diff([1,2],[1]) == [2]
+If a value is present in b, all of its occurrences must be removed from the other:
+
+array_diff([1,2,2,2,3],[2]) == [1,3]
+*/
+
+// with reduce
+function array_diff(a, b) {
+  return a.reduce((result, element) => {
+    if (!b.includes(element)) {
+    result.push(element) 
+    }
+    return result
+  }, [])
+}
+
+// with .forEach
+function array_diff(a, b) {
+  let result = []
+  a.forEach(element => {
+    if (!b.includes(element)) result.push(element)
+  })
+  return result
+}
